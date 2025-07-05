9b497fc5309b1341747ef16ac310cd71b499caee
ed-bayudan-dataacs <78091106+ed-bayudan-dataacs@users.noreply.github.com>
2025-07-05 12:48:28 -0400

    Update streamlit_app.py
    Change the API call to show info for ONLY the fruit chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ if ingredients_list:
     for fruit in ingredients_list:
         ingredients_string+= fruit
         ingredients_string+= " "
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit + ' Nutrition information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +   fruit)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     save_smoothie = st.button('Submit smoothie order')
