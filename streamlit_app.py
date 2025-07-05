c82a8fe6655bf17348fc32c4350d773ae8497d44
ed-bayudan-dataacs <78091106+ed-bayudan-dataacs@users.noreply.github.com>
2025-07-05 12:36:50 -0400

    Update streamlit_app.py
    Converted smoothiefroot response to JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ if ingredients_list:
         st.success(f"Your smoothie is ordered, {title}!", icon="✅")
         
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
